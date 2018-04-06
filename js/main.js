3f7d4048ec345c1c03e61785f6625ffb6d0093e9
dylanro <zopzzy2421@yahoo.com>
2018-04-06 09:07:18 -0600

    added cell class

--- js/main.js
+++ js/main.js
@@ -2,3 +2,38 @@ const rc = rough.canvas(document.getElementById('canvas'));
 rc.rectangle(10, 10, 200, 200);
 
 let cells = [...Array(10)].map(e => Array(10).fill(0));
+
+class cell {
+    construtor(x, y, w, h) {
+        this.top = true;
+        this.right = true;
+        this.bottom = true;
+        this.left = true;
+        this.width = w;
+        this.height = h;
+        this.x = x;
+        this.y = y;
+    }
+
+    removeEdge() {
+
+    }
+
+    display() {
+        if (top) {
+
+        }
+
+        if (right) {
+
+        }
+
+        if (bottom) {
+
+        }
+        
+        if (left) {
+
+        }
+    }
+}
