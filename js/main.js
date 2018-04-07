df6efdbdb0880818e2dd14453ee306f952aacf31
Dylan Roberts <31771652+dylanro@users.noreply.github.com>
2018-04-07 09:02:37 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
 const rc = rough.canvas(document.getElementById('canvas'));
 
 class Cell{
-    constructor(x, y, w, h){
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.w = w;
-        this.h = h;
+        this.w = 80;
+        this.h = 80;
         this.top = true;
         this.right = true;
         this.bottom = true;
@@ -31,8 +31,8 @@ class Cell{
         }
     }
     
-    removeEdge(num){
-        switch(num){
+    removeEdge(num) {
+        switch(num) {
             case 1: this.top = false; break;
             case 2: this.right = false; break;
             case 3: this.bottom = false; break;
@@ -43,14 +43,14 @@ class Cell{
 
 let cells = [];
 //let cells = [...Array(10)].map(e => Array(10).fill(0));
-for(let x = 10; x < 800; x+=80){//change the 800 for a variable so the size could possibly be changed within program
-    for(let y = 10; y < 800; y+=80){
-        cells.push(new Cell(x, y, 80, 80));
+for(let x = 10; x < 800; x+=80) {//change the 800 for a variable so the size could possibly be changed within program
+    for(let y = 10; y < 800; y+=80) {
+        cells.push(new Cell(x, y));
     }
 }
 
-for(let i = 0; i < cells.length; i++){
-    cells[i].removeEdge(1);//To test removing top edges
-    cells[i].removeEdge(3);//To test removing bottom edges
+for(let i = 0; i < cells.length; i++) {
+    //cells[i].removeEdge(1);//To test removing top edges
+    //cells[i].removeEdge(3);//To test removing bottom edges
     cells[i].display();
 }
