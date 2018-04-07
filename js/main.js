18f9d6609dd69b5743edf1ae0e524f517998d2ab
Dylan Roberts <31771652+dylanro@users.noreply.github.com>
2018-04-06 22:33:18 -0600

    Add files via upload
    cell objects are functional with a boolean for each side and a display function

--- js/main.js
+++ js/main.js
@@ -1,39 +1,46 @@
+"use strict"
 const rc = rough.canvas(document.getElementById('canvas'));
-rc.rectangle(10, 10, 200, 200);
 
-let cells = [...Array(10)].map(e => Array(10).fill(0));
-
-class cell {
-    construtor(x, y, w, h) {
+class Cell{
+    constructor(x, y, w, h){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
         this.top = true;
         this.right = true;
         this.bottom = true;
         this.left = true;
-        this.width = w;
-        this.height = h;
-        this.x = x;
-        this.y = y;
     }
 
-    removeEdge() {
-
-    }
-
-    display() {
-        if (top) {
-
+    display(){
+        if(this.top){
+            rc.line(this.x, this.y, this.x + this.w, this.y);
         }
 
-        if (right) {
-
+        if(this.right){
+            rc.line(this.x + this.w, this.y, this.x + this.w, this.y + this.h);
         }
 
-        if (bottom) {
-
+        if(this.bottom){
+            rc.line(this.x + this.w, this.y + this.h, this.x, this.y + this.h);
         }
-        
-        if (left) {
 
+        if(this.left){
+            rc.line(this.x, this.y + this.h, this.x, this.y);
         }
     }
+}
+
+let cells = [];
+//let cells = [...Array(10)].map(e => Array(10).fill(0));
+for(let x = 10; x < 800; x+=80){//change the 800 for a variable so the size could possibly be changed within program
+    for(let y = 10; y < 800; y+=80){
+        cells.push(new Cell(x, y, 80, 80));
+    }
+}
+
+//I read that .forEach is 95% slower than a regular for loop so I went with this
+for(let i = 0; i < cells.length; i++){
+    cells[i].display();
 }
