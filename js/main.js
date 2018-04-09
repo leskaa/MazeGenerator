478af9eb96d4cf751708c4da2690552f5e8c929b
Dylan Roberts <31771652+dylanro@users.noreply.github.com>
2018-04-08 20:05:35 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,9 +12,14 @@ class Cell {
         this.right = true;
         this.bottom = true;
         this.left = true;
+        this.visited = false;
+        this.neighbors = [];
+
+        this.xindex = this.x/80;//gives the numbers from 1-10
+        this.yindex = this.y/80;//gives the numbers 1-10
     }
 
-    display(){
+    display() {
         if(this.top) {
             rc.line(this.x, this.y, this.x + this.w, this.y);
         }
@@ -30,6 +35,19 @@ class Cell {
         if(this.left) {
             rc.line(this.x, this.y + this.h, this.x, this.y);
         }
+
+        if(this.visited){
+            rc.rectangle(this.x, this.y, this.w, this.h, {
+                fill: 'red'
+            });
+        }
+    }
+
+    getNeighbors(c) {
+        this.neighbors.push(1);//top
+        this.neighbors.push(2);//right
+        this.neighbors.push(3);//bottom
+        this.neighbors.push(4);//left
     }
     
     removeEdge(num) {
@@ -43,16 +61,28 @@ class Cell {
 }
 
 let cells = [];
+
 //let cells = [...Array(10)].map(e => Array(10).fill(0));
-for(let y = 10; y < 800; y+=80) {//change the 800 for a variable so the size could possibly be changed within program
-    for(let x = 10; x < 800; x+=80) {
+for(let y = 0; y < 800; y+=80) {//change the 800 for a variable so the size could possibly be changed within program
+    for(let x = 0; x < 800; x+=80) {
         cells.push(new Cell(x, y));
     }
 }
 
+let current = cells[0];
+current.visited = true;
+
+for(let i = 0; i < 20; i++){//for testing the algorithm
+    
+}
+
 //For displaying numbers above the cells for development reference(remove later)
-for(let i = 40; i < 800; i+=80) {
-    ctx.fillText(Math.round(i/80),i,7);
+for(let i = 80; i < 800; i+=80) {
+    ctx.fillText(Math.round(i/80),i+40,7);
+}
+
+for(let i = 80; i < 800; i+=80) {
+    ctx.fillText(Math.round(i/80),1,i+40);
 }
 
 for(let i = 0; i < cells.length; i++) {
