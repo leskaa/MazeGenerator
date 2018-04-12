576f9d33c8dc13fd8580f054a1446472be8b4933
dylanro <zopzzy2421@yahoo.com>
2018-04-11 21:56:36 -0500

    working on viewport

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,3 +1,7 @@
 $( '#fpsslider' ).change(function(){
   $( '#js-genspeed' ).text("Generation Speed: " + $( '#fpsslider' ).val() + " fps");
 });
+
+let width = window.innerWidth;
+$( '#js-width' ).val(width);
+$( '#js-height' ).val(width);
