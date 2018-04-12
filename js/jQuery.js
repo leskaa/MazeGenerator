99a1cbcb055db75abfa329476e904eaa5472903f
dylanro <zopzzy2421@yahoo.com>
2018-04-11 22:36:47 -0500

    finished viewport optimization

--- js/jQuery.js
+++ js/jQuery.js
@@ -3,5 +3,21 @@ $( '#fpsslider' ).change(function(){
 });
 
 let width = window.innerWidth;
-$( '#js-width' ).val(width);
-$( '#js-height' ).val(width);
+$( '#js-width' ).val(Math.floor(width));
+$( '#js-height' ).val(Math.floor(width));
+
+
+if(width < 480){
+  $( '#canvas' ).attr('width', width);
+  $( '#canvas' ).attr('height', width);
+}
+
+if(width > 480){
+  $( '#canvas' ).attr('width', width);
+  $( '#canvas' ).attr('height', width);
+}
+
+//$( '#canvas' )
+
+
+
